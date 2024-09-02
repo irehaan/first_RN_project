3fa49e1c8e7712524b049a072bdacfe76bb368a1
Rehan <muhammadrehan3024@gmail.com>
2024-09-02 11:25:48 +0500

    Navigation from index to profile page

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import { Slot, Stack } from 'expo-router';
 
-const App = () => {
+const Rootlayout = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Hello, World!</Text>
-    </SafeAreaView>
-  );
-};
+    <Stack>
+      <Stack.Screen name="index" options={{
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5f5f5',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333',
-  },
-});
+        headerShown: false  }} />
 
-export default App;
+    </Stack>
+ )
+}
+
+export default Rootlayout
+
+
